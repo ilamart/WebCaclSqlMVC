cd0707668cdcf8e763b3b8d21b7551b8d4cbc77f
ilamart <39222202+ilamart@users.noreply.github.com>
2018-07-10 16:13:04 +0300

    Add files via upload

---
+++ WebApplication1/Models/PageData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class PageData
+    {
+        public string NewSearchExpression { get; set; }
+        public string NewSearchHost { get; set; }
+
+        public string PreviousSearchExpression { get; set; }
+        public string PreviousSearchHost { get; set; }
+
+        public string Expression { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public List<History> Histories { get; set; }
+
+        public int PageCount { get; set; }
+        public int NotesPerPage = 10;
+        public int TotalPages { get; set; }
+        public int Count { get; set; }
+
+        public StringBuilder AnswerFilter { get; set; }
+        public StringBuilder BtnPrevious { get; set; }
+    }
+}
